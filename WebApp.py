d5f71713b89416bbe4d0824ba56b4f02109b1aab
kaushik Seetharam <kaushikseetharam989@gmail.com>
2020-04-16 17:33:13 +0530

    first commit

--- WebApp.py
+++ WebApp.py
@@ -1,11 +1,14 @@
-From flask import Flask
+from flask import Flask, render_template
+ 
 app = Flask(__name__)
-
+ 
+ 
 @app.route('/')
 def hello_world:
     return 'Hey, we have Flask in a Docker container!'
-
-
-if __name == '__main__':
+ 
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    
+    
  
