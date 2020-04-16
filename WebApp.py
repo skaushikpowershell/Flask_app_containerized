72c6a8f8ca098cb8e8e2e81a7348e9f6c9420ac5
kaushik Seetharam <kaushikseetharam989@gmail.com>
2020-04-16 17:24:47 +0530

    first commit

--- WebApp.py
+++ WebApp.py
@@ -8,7 +8,7 @@ def home():
     return render_template("login.html")
  
 if __name__ == '__main__':
-    app.run(debug=True, host='18.217.234.114')
+    app.run(debug=True, host='0.0.0.0')
     
     
  
